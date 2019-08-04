9c5fdc398d096b0c74c50b5d905d967369cc3013
Brad Smith <brad@smithwebtek.com>
2019-08-04 14:34:20 -0400

    Add form submission

--- expresstut.js
+++ expresstut.js
@@ -9,7 +9,13 @@ var handlebars = require('express-handlebars').create({ defaultLayout: 'main' })
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-// MORE IMPORTS HERE
+// IMPORTS
+app.use(require('body-parser').urlencoded({ extended: true }));
+
+var formidable = require('formidable');
+
+var credentials = require('./credentials.js');
+app.use(require('cookie-parser')(credentials.cookieSecret));
 
 app.set('port', process.env.PORT || 3000);
 
@@ -31,19 +37,23 @@ app.get('/about', function (req, res) {
 });
 
 app.get('/contact', function (req, res) {
-  res.render('contact');
+  res.render('contact', { csrf: 'CSRF token here' });
 });
 
-app.post('/', function (req, res) {
-  // console.log("contact post request : ", req)
+app.get('/thankyou', function (req, res) {
+  res.render('thankyou');
 });
 
-app.get('/upload', function (req, res) {
-  app.render('upload-file');
+app.post('/process', function (req, res) {
+  console.log('Form : ' + req.query.form);
+  console.log('CRSF token : ' + req.body._csrf);
+  console.log('Email : ' + req.body.email);
+  console.log('Question : ' + req.body.ques);
+  res.redirect(303, '/thankyou');
 });
 
+// ERRORS and handling
 app.use(function (req, res, next) {
-  console.log('app.use function was called')
   console.log("Looking for URL : ", req.url);
   next();
 });
@@ -58,6 +68,18 @@ app.use(function (err, req, res, next) {
   console.log('Error : ', err.message);
   next();
 })
+
+app.use(function (req, res) {
+  res.type('text/html');
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function (req, res, next) {
+  res.type('text/html');
+  res.status(500);
+  res.render('500');
+});
 
 app.listen(app.get('port'), function () {
   console.log("Express started on http://localhost:" + app.get('port') + " press Ctrl-C to terminate");
